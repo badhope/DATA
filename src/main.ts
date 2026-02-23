fbce9ec9328f9e83d1c90cba0349baebba4b862d
bigbad <x18825407105@outlook.com>
2026-02-23 23:04:49 +0800

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,13 @@ import router from './router';
 import pinia from './store';
 import Antd from 'ant-design-vue';
 import 'ant-design-vue/dist/reset.css';
-import './mock'; // 引入 Mock 数据
+import './styles/global.css';
+import { setupMock } from './mock';
 
 const app = createApp(App);
+
+// 启动 Mock 数据
+setupMock();
 
 app.use(pinia);
 app.use(router);
